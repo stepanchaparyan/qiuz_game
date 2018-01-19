0ad92cee5525d6756214d75d0848e26cc68b116a
stepanchaparyan <schaparian@yahoo.com>
2018-01-20 02:15:49 +0400

    19.01.18 cleanup in git folder

--- green_capital/data.js
+++
@@ -1,9 +0,0 @@
-var y = {
-    "name":"Jogggghn",
-    "age":31,
-    "pets":[
-        { "animal":"dog", "name":"Fido" },
-        { "animal":"cat", "name":"Felix" },
-        { "animal":"hamster", "name":"Lightning" }
-    ]
-}
