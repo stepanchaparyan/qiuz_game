440a38c3fa7546d2678457d8821d3a9ba0195156
Stephan <schaparian@yahoo.com>
2018-01-22 19:08:55 +0400

    delete unnecessory file and add some comments in structure file

--- capitals_js/js/index.js
+++ capitals_js/js/index.js
@@ -1,74 +1,7 @@
-//creates variable for counter and sets it to zero to start.
-var quizScore = 0;
+// variables for question number and score
 
-//Creates answer bank to test responses to.
-var questionOneAnswer = 'HARRISBURG';
-var questionTwoAnswer = 'COLUMBUS';
-var questionThreeAnswer = 'ALBANY';
-var questionFourAnswer = 'PROVIDENCE';
-var questionFiveAnswer = 'BOSTON';
-
-function quiz () {
-//Creates variable for holding what the person guesses as the answer.
-//Also changes their guess to uppercase to avoid any capitalization errors.
-//Tests the guess against the answer and increases score if they get it right.
-var questionOneGuess = prompt('Question 1: What is the capital of Pennsylvania?');
-if (questionOneGuess.toUpperCase() === questionOneAnswer) {
-quizScore = quizScore + 1;
-alert('That is correct!');
-} else {
-alert('Sorry that is incorrect.');
-}
-
-var questionTwoGuess = prompt('Question 2: What is the capital of Ohio?');
-if (questionTwoGuess.toUpperCase() === questionTwoAnswer) {
-quizScore = quizScore + 1;
-alert('That is correct!');
-} else {
-alert('Sorry that is incorrect.');
-}
-
-var questionThreeGuess = prompt('Question 3: What is the capital of New York?');
-if (questionThreeGuess.toUpperCase() === questionThreeAnswer) {
-quizScore= quizScore + 1;
-alert('That is correct!');
-} else {
-alert('Sorry that is incorrect.');
-}
-
-var questionFourGuess = prompt('Question 4: What is the capital of Rhode Island?');
-if (questionFourGuess.toUpperCase() === questionFourAnswer) {
-quizScore = quizScore + 1;
-alert('That is correct!');
-} else {
-alert('Sorry that is incorrect.');
-}
-
-var questionFiveGuess = prompt('Question 5: What is the capital of Massachusetts?');
-if (questionFiveGuess.toUpperCase() === questionFiveAnswer) {
-quizScore = quizScore + 1;
-alert('That is correct!');
-} else {
-alert('Sorry that is incorrect.');
-}
-}
-
-function results () {
-//Determines the player's score by testing how many they got correct.
-if (quizScore === 5) {
-document.write('<p>You win the gold crown with a score of ' + quizScore + ' correct answers.</p>');
-} else if (quizScore === 4 || quizScore === 3) {
-document.write('<p>You win the silver crown with a score of ' + quizScore + ' correct answers.</p>');
-} else if (quizScore === 2 || quizScore === 1) {
-document.write('<p>You win the bronze crown with a score of ' + quizScore + ' correct answers.</p>');
-} else {
-document.write("<p>You haven't won any crown. You answered " + quizScore + " questions correctly.</p>");
-}
-}
-
-
-
-
+let questionNumber = 0;
+let score = 0;
 
 function hideAndShow() {
   document.getElementById('start-page').style.display='none';
@@ -79,9 +12,6 @@ function hideAndShow() {
   document.getElementById('result-message').style.display='block';
   document.getElementById('result-step-score').style.display='block';
 }
-
-let questionNumber = 0;
-let score = 0;
 
 function questions() {
   document.getElementById("quiz-questions").innerHTML = "The capital city of " + COUNTRIES[questionNumber].name + " is " + COUNTRIES[questionNumber].capital;
@@ -121,5 +51,4 @@ function start () {
   hideAndShow();
   questions();
   result();
-
 }
