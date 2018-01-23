bdeb60a0ae70a01eac58638be3ea6310eb86e345
Stephan <schaparian@yahoo.com>
2018-01-23 20:04:59 +0400

    add functionality to random question right or wrong

--- capitals_js/js/index.js
+++ capitals_js/js/index.js
@@ -1,8 +1,11 @@
 // variables for question number and score
-
 let questionNumber = 0;
 let score = 0;
+let randomNumberMain = Math.floor(Math.random() * Math.floor(2));
+let randomNumber1 = Math.floor(Math.random() * 5) + 1;
+let randomNumber2 = Math.floor(Math.random() * 10) + 7;
 
+//function for hide first card and show question cards
 function hideAndShow() {
   document.getElementById('start-page').style.display='none';
   document.getElementById('quiz-questions').style.display='block';
@@ -12,12 +15,28 @@ function hideAndShow() {
   document.getElementById('result-step-score').style.display='block';
 }
 
-function questions() {
-  document.getElementById("quiz-questions").innerHTML = "The capital city of " + COUNTRIES[questionNumber].name + " is " + COUNTRIES[questionNumber].capital;
+function questionsMain () {
+  randomNumberMain = Math.floor(Math.random() * Math.floor(2));
+  if (randomNumberMain == 1) {
+    questionsRight();
+  } else {
+    questionsWrong();
+  }
+  console.log(randomNumberMain);
+  console.log(randomNumber1);
+  console.log(randomNumber2);
+}
+
+function questionsWrong() {
+  document.getElementById("quiz-questions").innerHTML = "The capital city of " + COUNTRIES[randomNumber1].name + " is " + COUNTRIES[randomNumber2].capital;
+}
+
+function questionsRight() {
+  document.getElementById("quiz-questions").innerHTML = "The capital city of " + COUNTRIES[randomNumber1].name + " is " + COUNTRIES[randomNumber1].capital;
 }
 
 function testRight () {
-  if (COUNTRIES[questionNumber].answer == "right") {
+  if (randomNumberMain == 1) {
     document.getElementById("result-message").innerHTML = "You are right";
     document.getElementById('right').style.borderColor = "#48b484";
     document.getElementById('result-message').style.borderColor = "#48b484";
@@ -29,17 +48,16 @@ function testRight () {
     document.getElementById('result-message').style.borderColor = "red";
   }
     document.getElementById('result-message').style.display='block';
-    document.getElementById('result-message').style.borderColor = "#48b484";
     document.getElementById('btn-next').disabled = false;
 }
 
 function testWrong () {
-  if (COUNTRIES[questionNumber].answer == "right") {
-    document.getElementById("result-message").innerHTML = "Sorry, but " + COUNTRIES[questionNumber].capital + " is the capital of " + COUNTRIES[questionNumber].name;
+  if (randomNumberMain == 1) {
+    document.getElementById("result-message").innerHTML = "Sorry, but " + COUNTRIES[randomNumber1].capital + " is the capital of " + COUNTRIES[randomNumber1].name;
     document.getElementById('wrong').style.borderColor = "red";
     document.getElementById('result-message').style.borderColor = "red";
   } else {
-    document.getElementById("result-message").innerHTML = "You are right, " + COUNTRIES[questionNumber].capital + "is not the capital of " + COUNTRIES[questionNumber].name;
+    document.getElementById("result-message").innerHTML = "You are right, " + COUNTRIES[randomNumber1].capital + "is not the capital of " + COUNTRIES[randomNumber2].name;
     document.getElementById('wrong').style.borderColor = "#48b484";
     score += 1;
     result();
@@ -55,6 +73,8 @@ function result () {
 
 function next () {
   questionNumber += 1;
+  randomNumber1 = Math.floor(Math.random() * 5) + 1;
+  randomNumber2 = Math.floor(Math.random() * 10) + 6;
   document.getElementById('right').style.borderColor = "#b5d2d3";
   document.getElementById('wrong').style.borderColor = "#b5d2d3";
   document.getElementById('result-message').style.display='none';
@@ -64,6 +84,6 @@ function next () {
 
 function start () {
   hideAndShow();
-  questions();
+  questionsMain();
   result();
 }
