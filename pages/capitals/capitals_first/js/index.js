a202e9ce6fa82068315e4e8cb2ddc2ddd1b89d22
Stephan <schaparian@yahoo.com>
2018-02-06 19:23:53 +0400

    fix Points update issue, update lokijs functions, change functions to arrow functions

--- pages/capitals/capitals_first/js/index.js
+++ pages/capitals/capitals_first/js/index.js
@@ -8,9 +8,8 @@ let randomNumber;
 let randomNumberExcluded;
 let countries_list;
 
-
 //function for hide first card and show question cards
-function hideAndShow() {
+let hideAndShow = () => {
   document.getElementById('start-page-world').setAttribute("class", "hide-display");
   document.getElementById('start-page-europe').setAttribute("class", "hide-display");
   document.getElementById('start-page-asia').setAttribute("class", "hide-display");
@@ -26,24 +25,25 @@ function hideAndShow() {
 }
 
 //generate and print right or wrong question
-function questionsMain () {
+let questionsMain = () => {
   randomNumberMain = Math.floor(Math.random() * Math.floor(2));
   randomNumberMain == 1 ? questionsRight() : questionsWrong();
   console.log(randomNumberMain);
 }
 
 //print Wrong question
-function questionsWrong() {
-  document.getElementById("quiz-questions").innerHTML = "The capital city of " + countries_list[randomNumber].name + " is " + countries_list[randomNumberExcluded].capital;
-}
+let questionsWrong = () => document.getElementById("quiz-questions").innerHTML = "The capital city of " + countries_list[randomNumber].name + " is " + countries_list[randomNumberExcluded].capital;
 
 //print Right question
-function questionsRight() {
-  document.getElementById("quiz-questions").innerHTML = "The capital city of " + countries_list[randomNumber].name + " is " + countries_list[randomNumber].capital;
-}
+let questionsRight = () => document.getElementById("quiz-questions").innerHTML = "The capital city of " + countries_list[randomNumber].name + " is " + countries_list[randomNumber].capital;
+
+//print Right answer
+let addRightAnswer = () => document.getElementById("result-message").innerHTML = "Right answer: " + countries_list[randomNumber].capital + " is the capital of " + countries_list[randomNumber].name;
+
+let getFocus = () => document.getElementById("btn-next").focus();
 
 //generate CSS for right or wrong answer
-function setNewCSS(id, color) {
+let setNewCSS = (id, color) => {
   if (color == "green") {
     document.getElementById(id).setAttribute("class", "greenBorder");
     document.getElementById('result-message').setAttribute("class", "greenBorder");
@@ -55,41 +55,32 @@ function setNewCSS(id, color) {
   }
 }
 
-//print Right answer
-function addRightAnswer() {
-  document.getElementById("result-message").innerHTML = "Right answer: " + countries_list[randomNumber].capital + " is the capital of " + countries_list[randomNumber].name;
-}
-
 //change element from disabled to abled and vice versa
-function changeDisabled() {
+let changeDisabled = () => {
   document.getElementById('btn-next').disabled = !document.getElementById('btn-next').disabled;
   document.getElementById('wrong').disabled = !document.getElementById('wrong').disabled;
   document.getElementById('right').disabled = !document.getElementById('right').disabled;
 }
 
-function getFocus() {
-  document.getElementById("btn-next").focus();
-}
-
-function testRight () {
+let testRight = () => {
   randomNumberMain == 1 ? (setNewCSS("right", "green"), score += 1, result()) : (setNewCSS("right", "red"), addRightAnswer());
   changeDisabled();
   getFocus();
 }
 
-function testWrong () {
+let testWrong = () => {
   randomNumberMain == 1 ? (setNewCSS("wrong", "red")) : (setNewCSS("wrong", "green"), addRightAnswer(), score += 1, result());
   changeDisabled();
   getFocus();
 }
 
 //print score and question number
-function result() {
+let result = () => {
   document.getElementById("result-step").innerHTML = " Question: " + (questionNumber + 1) + " /10";
   document.getElementById("result-score").innerHTML = " Score: " + score + " /10";
 }
 
-function finalResult() {
+let finalResult = () => {
   if (questionNumber == 5) {
   document.getElementById("final-score").innerHTML = "You win " + score + " points";
   document.getElementById('main').setAttribute("class", "hide-display");
@@ -97,32 +88,13 @@ function finalResult() {
   }
 }
 
-
-var point;
-function testStorage() {
-  if (localStorage.getItem("point") == undefined) {
-    localStorage.setItem("point", 0);
-    console.log("storae1 " + localStorage.getItem("point"));
-  } else {
-    //localStorage.removeItem("point");
-    point = localStorage.getItem("point");
-    point = Number(point) + score;
-    console.log("point " + point);
-    localStorage.setItem("point", point);
-    console.log("storage2 " + localStorage.getItem("point"));
-    console.log("point2  " + point);
-    document.getElementById('point').innerHTML = point;
-  }
-}
-
-
-function tryAgain() {
+let tryAgain = () => {
   addPoints();
   document.location.reload();
 }
 
 //generateRandum numbers for question
-function setRandomNumbers(continent) {
+let setRandomNumbers = (continent) => {
   if(continent == "Asia") {
     countries_list = COUNTRIES_ASIA;
   } else if (continent == "Europe") {
@@ -136,19 +108,19 @@ function setRandomNumbers(continent) {
   } else {
     countries_list = COUNTRIES;
   }
-    randomNumber = Math.floor(Math.random() * countries_list.length-1) + 1;
-    randomNumberExcluded = randomExcluded(0, countries_list.length-1, randomNumber);
+  randomNumber = Math.floor(Math.random() * countries_list.length-1) + 1;
+  randomNumberExcluded = randomExcluded(0, countries_list.length-1, randomNumber);
 }
 
 //generate randum number Excluded question number
-function randomExcluded(min, max, excluded) {
+let randomExcluded = (min, max, excluded) => {
   let n = Math.floor(Math.random() * (max-min) + min);
   if (n >= excluded) n++;
   return n;
 }
 
 //list of functions/action for next button
-function next() {
+let next = () => {
   questionNumber += 1;
   document.getElementById('right').removeAttribute("class");
   document.getElementById('wrong').removeAttribute("class");
@@ -158,7 +130,7 @@ function next() {
   chooseContinent();
 }
 
-function chooseContinent() {
+let chooseContinent = () => {
   if(countries_list == COUNTRIES_EUROPE) {
     start("Europe");
   } else if (countries_list == COUNTRIES_ASIA) {
@@ -176,18 +148,18 @@ function chooseContinent() {
 
 // evropa 47  // asia 49  // africa 59  // americas 56 (28)  // oceania 27
 
-function start(continent) {
+let start = (continent) => {
   setRandomNumbers(continent);
   startAll();
 }
 
-function startAll() {
+let startAll = () => {
   hideAndShow();
   questionsMain();
   result();
   finalResult();
 }
 
-function loadFunction() {
+let loadFunction = () => {
   document.getElementById('point').innerHTML = points.data[0].CapitalPoints;
 }
--- pages/capitals/capitals_first/js/loki.js
+++ pages/capitals/capitals_first/js/loki.js
@@ -2,39 +2,29 @@
  var db = new loki('loki.db');
 
  db.loadDatabase({}, function() {
+ points = db.addCollection('Points');
  points = db.getCollection('Points');
- if (points.data.length === 0) {
+ //points.removeDataOnly()
+ if (points.data.length === 0 || points.data.length === null) {
+   console.log("points.data.length =  " + points.data.length);
    points.insert({
+        Name: "",
+        Surname: "",
         MainPoints: 0,
         CapitalPoints: 0,
         FlagPoints: 0
       });
    db.saveDatabase();
- } else {
-   //points.removeDataOnly();
-   db.saveDatabase();
-  };
-  });
+ } else if (points.data[0].CapitalPoints > 5) {
+   points.data[0].CapitalPoints = 0;
+ db.saveDatabase();
+ };
+ });
 
-
- var addPoints = function() {
- var coll = db.getCollection('Points');
- if (coll === null) {
-   points = db.addCollection('Points');
-   points.insert({
-     MainPoints: 0,
-     CapitalPoints: 0,
-     FlagPoints: 0
-   });
-   db.saveDatabase();
- } else {
+ let addPoints = () => {
  db.loadDatabase({}, function () {
  points = db.getCollection('Points');
- var tempScore = 0;
- tempScore = points.data[0].CapitalPoints;
- var tempScore2 = tempScore + score;
- points.data[0].CapitalPoints = tempScore2;
+ points.data[0].CapitalPoints = points.data[0].CapitalPoints + score;
  db.saveDatabase();
  });
- }
  }
