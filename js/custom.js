8877b1b8692768fa0488c6d5199fb876c42726ed
stepanchaparyan <schaparian@yahoo.com>
2018-02-08 01:52:13 +0400

    add login part in main page, made little UI updates in main page

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,12 @@
 (function ($) {
-    
+
     // Init Wow
     wow = new WOW( {
         animateClass: 'animated',
         offset:       100
     });
     wow.init();
-    
+
     // Navigation scrolls
     $('.navbar-nav li a').bind('click', function(event) {
         $('.navbar-nav li').removeClass('active');
@@ -14,14 +14,14 @@
         var $anchor = $(this);
         var nav = $($anchor.attr('href'));
         if (nav.length) {
-        $('html, body').stop().animate({				
-            scrollTop: $($anchor.attr('href')).offset().top				
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
-        
+
         event.preventDefault();
         }
     });
-    
+
     // About section scroll
     $(".overlay-detail a").on('click', function(event) {
         event.preventDefault();
@@ -32,7 +32,7 @@
             window.location.hash = hash;
         });
     });
-    
+
     //jQuery to collapse the navbar on scroll
     $(window).scroll(function() {
         if ($(".navbar-default").offset().top > 50) {
@@ -41,14 +41,12 @@
             $(".navbar-fixed-top").removeClass("top-nav-collapse");
         }
     });
-    
+
     // Testimonials Slider
     $('.bxslider').bxSlider({
       adaptiveHeight: true,
       mode: 'fade'
     });
 
-    
+
 })(jQuery);
-
-
